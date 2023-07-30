d451f7b157c949fe9842180d5259ab4fd60e5358
yshen92 <yshen_92@hotmail.com>
2023-07-30 22:59:02 +0800

    Added unit and integration tests

---
+++ tests/integration_test.py
@@ -0,0 +1,28 @@
+from ..deployment import inference
+
+import boto3
+
+def check_s3_file_exists(bucket_name, object_key):
+    s3 = boto3.resource('s3')
+    try:
+        s3.Object(bucket_name, object_key).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            return False
+        else:
+            raise
+    else:
+        return True
+
+def TestIntegration(mlflow_tracking_uri):
+
+    inference.spam_detection(mlflow_tracking_uri)
+    year, month = inference.get_current_year_and_month()
+    inference_output_exists = check_s3_file_exists('mlops-capstone-prediction', f'year={year:04d}/month={month:02d}/spam_detection.parquet')
+
+    assert inference_output_exists == True, 'Inference output file does not exist'
+
+    print('Integration test passed')
+
+if __name__ == '__main__':
+    TestIntegration(sys.argv[1]) 
---
+++ tests/unit_tests.py
@@ -0,0 +1,37 @@
+from deployment import inference
+
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+
+def test_text_embeddings_length():
+    test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    
+    exptected_embedding_len = 768
+
+    sentence_transformer = inference.load_preprocessor()
+    embeddings = inference.embed_text(test_text, sentence_transformer)
+
+    assert len(embeddings) == exptected_embedding_len, 'Embedding lengths are not equal'
+
+    print('Embedding length test passed')
+
+def test_text_embeddings():
+    test_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    inference_transformer = inference.load_preprocessor()
+    inference_embeddings = inference.embed_text(test_text, inference_transformer)
+
+    expected_transformer = SentenceTransformer('all-mpnet-base-v2')
+    expected_embeddings = expected_transformer.encode(test_text)
+
+    inference_embeddings = np.round(inference_embeddings, decimals=3)
+    expected_embeddings = np.round(expected_embeddings, decimals=3)
+
+    assert (inference_embeddings == expected_embeddings).all(), 'Embedding values are not equal'
+
+    print('Embedding values test passed')
+
+if __name__ == '__main__':
+    test_text_embeddings_length() 
+    test_text_embeddings()
